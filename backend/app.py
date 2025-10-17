77ec59e69dde3322809427d092ab2cb370f0268a
Titus Kiplagat <tituskipkoech737@gmail.com>
2025-10-17 13:40:40 +0300

    Update CORS origins in app.py

--- backend/app.py
+++ backend/app.py
@@ -27,7 +27,7 @@ CORS(
         "http://localhost:8080",
         "http://localhost:5173",
         "http://127.0.0.1:8080",
-        "https://clients.savannadesignsagency.com",
+        "https://syntheticindicesreview.com",
         "https://syntheticspro.vercel.app"
     ]
 )
